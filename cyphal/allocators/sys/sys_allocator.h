8bd829e92ccd540cdfa78ff2e651a632cb905339
Igor Beschastnov <beschastnovigor@gmail.com>
2024-01-22 18:54:32 +0300

    bugfix (sys explicit constructor)

--- cyphal/allocators/sys/sys_allocator.h
+++ cyphal/allocators/sys/sys_allocator.h
@@ -4,6 +4,8 @@
 
 class SystemAllocator : public AbstractAllocator {
 public:
+	// TODO: do something with size value?
+	explicit SystemAllocator(size_t size): AbstractAllocator(size) {};
     void* allocate(CanardInstance* ins, size_t amount) override;
     void free(CanardInstance* ins, void* pointer) override;
 };
