25b03e72f790722d531ef6ebb9818df14e202c0e
Igor Beschastnov <i.beschastnov@focuslabs.ru>
2026-02-18 17:44:49 +0300

    Add register access helper

--- cyphal/node/registers_access.hpp
+++ cyphal/node/registers_access.hpp
@@ -2,12 +2,15 @@
 
 #include <optional>
 #include <functional>
+#include <tuple>
+#include <map>
 
 #include <cyphal/node/registers_handler.hpp>
 #include <cyphal/node/registers_utils.hpp>
 
 class RegistersProxy: public AbstractSubscription<RegisterAccessResponse> {
 private:
+    std::map<CanardTransferID, std::tuple<std::string, uint64_t>> transfer_map;
     const CanardNodeID target_node_id;
     CanardTransferID register_access_transfer_id = 0;
 
@@ -23,8 +26,30 @@ private:
         else {
             request.value._tag_ = REGISTER_EMPTY_TAG;
         }
-        send_access_request(&request);
+
+        clean_transfer_map();
+        transfer_map[register_access_transfer_id] = std::make_tuple(
+            name,
+            interface->get_utilities().micros_64() + DEFAULT_TIMEOUT_MICROS
+        );
+        interface->send_request<RegisterAccessRequest>(
+            &request,
+            uavcan_register_Access_1_0_FIXED_PORT_ID_,
+            &register_access_transfer_id,
+            target_node_id
+        );
     }
+
+    void clean_transfer_map() {
+        std::erase_if(
+            transfer_map,
+            [this](const auto& item) {
+                const auto& [key, val] = item;
+                return interface->get_utilities().micros_64() > std::get<uint64_t>(val);
+            }
+        );
+    }
+
 public:
     RegistersProxy(
         InterfacePtr interface,
@@ -38,20 +63,15 @@ public:
         target_node_id(target_node_id)
     {}
 
-    void send_access_request(RegisterAccessRequest::Type* request) {
-        interface->send_request<RegisterAccessRequest>(
-            request,
-            uavcan_register_Access_1_0_FIXED_PORT_ID_,
-            &register_access_transfer_id,
-            target_node_id
-        );
+    size_t pending_requests() {
+        return transfer_map.size();
     }
 
-    void request_real32_value(const std::string& name, std::optional<float> value) {
+    void request_real32_value(const std::string& name, std::optional<float> value = std::nullopt) {
         request_generic<float>(name, value, &fill_register_real32);
     }
 
-    void request_natural32_value(const std::string& name, std::optional<uint32_t> value) {
+    void request_natural32_value(const std::string& name, std::optional<uint32_t> value = std::nullopt) {
         request_generic<uint32_t>(name, value, &fill_register_natural32);
     }
 
@@ -59,18 +79,24 @@ public:
         if (transfer->metadata.remote_node_id != target_node_id) {
             return;
         }
+        CanardTransferID key = transfer->metadata.transfer_id;
+        if (transfer_map.count(key) == 0) {
+            return;
+        }
+        const std::string& reg_name = std::get<std::string>(transfer_map[key]);
         switch (register_response.value._tag_) {
             case REGISTER_REAL32_TAG:
-                handle_float32(register_response.value.real32.value.elements[0], transfer);
+                handle_float32(reg_name, register_response.value.real32.value.elements[0], transfer);
                 break;
             case REGISTER_NATURAL32_TAG:
-                handle_natural32(register_response.value.natural32.value.elements[0], transfer);
+                handle_natural32(reg_name, register_response.value.natural32.value.elements[0], transfer);
                 break;
             default:
                 break;
         }
+        transfer_map.erase(key);
     };
 
-    virtual void handle_float32(float value, CanardRxTransfer* transfer) = 0;
-    virtual void handle_natural32(uint32_t value, CanardRxTransfer* transfer) = 0;
+    virtual void handle_float32(const std::string& name, float value, CanardRxTransfer* transfer) = 0;
+    virtual void handle_natural32(const std::string& name, uint32_t value, CanardRxTransfer* transfer) = 0;
 };
