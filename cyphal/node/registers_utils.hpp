84d1b8c466a9c55c6785db7c28df1274c3fdff1b
lulko <keklolov7@gmail.com>
2026-04-06 11:18:11 +0300

    add angle_direction

--- cyphal/node/registers_utils.hpp
+++ cyphal/node/registers_utils.hpp
@@ -3,6 +3,7 @@
 #include <cyphal/node/registers_handler.hpp>
 
 static constexpr uint8_t REGISTER_EMPTY_TAG = 0U;
+static constexpr uint8_t REGISTER_INTEGER32_TAG = 5U;
 static constexpr uint8_t REGISTER_BIT_TAG = 3U;
 static constexpr uint8_t REGISTER_REAL32_TAG = 13U;
 static constexpr uint8_t REGISTER_NATURAL32_TAG = 9U;
@@ -23,6 +24,14 @@ inline bool parse_register_real32(const uavcan_register_Value_1_0& value, float&
     return true;
 }
 
+inline bool parse_register_integer32(const uavcan_register_Value_1_0& value, int32_t& parsed) {
+    if (value._tag_ != REGISTER_INTEGER32_TAG || value.integer32.value.count == 0) {
+        return false;
+    }
+    parsed = value.integer32.value.elements[0];
+    return true;
+}
+
 inline void fill_register_bit(uavcan_register_Value_1_0& out, bool value) {
     out._tag_ = REGISTER_BIT_TAG;
     out.bit.value.bitpacked[0] = value;
@@ -33,6 +42,12 @@ inline void fill_register_real32(uavcan_register_Value_1_0& out, float value) {
     out._tag_ = REGISTER_REAL32_TAG;
     out.real32.value.elements[0] = value;
     out.real32.value.count = 1;
+}
+
+inline void fill_register_integer32(uavcan_register_Value_1_0& out, int32_t value) {
+    out._tag_ = REGISTER_INTEGER32_TAG;
+    out.integer32.value.elements[0] = value;
+    out.integer32.value.count = 1;
 }
 
 inline void fill_register_natural32(uavcan_register_Value_1_0& out, uint32_t value) {
